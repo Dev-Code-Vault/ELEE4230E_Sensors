bebc7329050c0fb69874115315ec642f6da6bd09
Dev-Code-Vault <study.vault@proton.me>
2026-06-23 02:04:40 -0400

    Add I2C scanner for MPU6050 detection

--- firmware/esp32_serial_test/esp32_serial_test.ino
+++ firmware/esp32_serial_test/esp32_serial_test.ino
@@ -1,32 +1,9 @@
-#include <Wire.h>
-
 void setup() {
-  Wire.begin(21, 22);
   Serial.begin(115200);
-  delay(1000);
-  Serial.println("Scanning...");
+  Serial.println("ESP32 STARTED");
 }
 
 void loop() {
-  byte error, address;
-  int nDevices = 0;
-
-  for (address = 1; address < 127; address++) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0) {
-      Serial.print("I2C device found at 0x");
-      if (address < 16) Serial.print("0");
-      Serial.println(address, HEX);
-      nDevices++;
-    }
-  }
-
-  if (nDevices == 0) {
-    Serial.println("No devices found");
-  }
-
-  Serial.println("Scan complete.");
-  delay(5000);
+  Serial.println("Running...");
+  delay(1000);
 }
