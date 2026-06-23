e2e17a8483cf6d33a4a08e0e12b9922b3a6cbfa1
Dev-Code-Vault <study.vault@proton.me>
2026-06-23 02:07:24 -0400

    Add MPU6050 angle reading firmware

---
+++ firmware/mpu6050_angle_reading/mpu6050_angle_reading.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);
+
+  if (!mpu.begin()) {
+    Serial.println("MPU6050 not found");
+    while (1) delay(10);
+  }
+
+  Serial.println("time_ms,accel_x,accel_y,accel_z,angle_deg");
+}
+
+void loop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  float angle_deg = atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI;
+
+  Serial.print(millis());
+  Serial.print(",");
+  Serial.print(a.acceleration.x);
+  Serial.print(",");
+  Serial.print(a.acceleration.y);
+  Serial.print(",");
+  Serial.print(a.acceleration.z);
+  Serial.print(",");
+  Serial.println(angle_deg);
+
+  delay(200);
+}
